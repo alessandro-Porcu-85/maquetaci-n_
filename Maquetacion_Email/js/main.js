b5998174ed5f1d76498a2621ceb09a77a2f4ab4f
alessandro-Porcu-85 <34552726+alessandro-Porcu-85@users.noreply.github.com>
2021-10-30 12:43:22 +0200

    Add files via upload

---
+++ Maquetacion_Email/js/main.js
@@ -0,0 +1,50 @@
+//Leer mas...
+
+$(document).ready(function() {
+    // Configure/customize these variables.
+    var showChar = 100; // How many characters are shown by default
+    var ellipsestext = "...";
+    var moretext = "Leer menos >";
+    var lesstext = "Leer màs >";
+
+
+    $('.more').each(function() {
+        var content = $(this).html();
+
+        if (content.length > showChar) {
+
+            var c = content.substr(0, showChar);
+            var h = content.substr(showChar, content.length - showChar);
+
+            var html = c + '<span class="moreellipses">' + ellipsestext + '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+
+            $(this).html(html);
+        }
+
+    });
+
+    $(".morelink").click(function() {
+        if ($(this).hasClass("less")) {
+            $(this).removeClass("less");
+            $(this).html(moretext);
+        } else {
+            $(this).addClass("less");
+            $(this).html(lesstext);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
+    });
+
+    $(document).ready(function() {
+        $nav = $('.container-fluid');
+        $toggleCollapse = $('.toggle-collapse');
+
+        /*click-event on toggle menu*/
+        $toggleCollapse.click(function() {
+            $nav.toggleClass('collapse');
+        });
+    });
+
+
+});
